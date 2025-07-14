fc836d4a50438763790e35474f99eb9d5d9ff14d
Jaco du Plessis <jaco@jacoduplessis.co.za>
2025-07-14 14:51:06 +0200

    Make project compatible with uv

--- defects/reports.py
+++ defects/reports.py
@@ -1,25 +1,9 @@
-from weasyprint import default_url_fetcher
 from django.core.files.storage import default_storage
 from django.contrib.staticfiles.finders import find
 import importlib.resources
 from pptx import Presentation
 from django.utils.timezone import now
 
-
-def url_fetcher(url, timeout=5, ssl_context=None):
-    # handle local media
-    if url.startswith("local:"):
-        path = url[6:]
-        file_obj = default_storage.open(path, "rb")
-        return {
-            "file_obj": file_obj,
-        }
-    if url.startswith("static:"):
-        path = url[7:]
-        abs_path = find(path)
-        return {"file_obj": open(abs_path, "rb")}
-
-    return default_url_fetcher(url, timeout=timeout, ssl_context=ssl_context)
 
 
 def render_pptx(target):
